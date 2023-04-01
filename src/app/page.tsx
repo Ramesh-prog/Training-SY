bfe863316f7c449eeed13c0fe85d90155c77adbe
Ramesh Mohapatra <rameshmohapatra2078@gmail.com>
2023-04-01 18:50:19 +0530

    2nd commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+export default async function Home() {
+  const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
+  const res = await data.json()
+  console.log(res) 
   return (
     <main>
       <h1>Hello next 13 </h1>
